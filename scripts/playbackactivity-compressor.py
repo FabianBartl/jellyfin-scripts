8c5d44a5c6a67f5d293e913be2a2d2a7e18a5467
Fabian <fabian@informatic-freak.de>
2026-04-10 12:32:24 +0200

    add logging
    protect compressed playback entries from beeing capped
    
    ignore db files

--- scripts/playbackactivity-compressor.py
+++ scripts/playbackactivity-compressor.py
@@ -14,6 +14,13 @@ import datetime as dt
 from pathlib import Path
 from dataclasses import dataclass
 from typing import Any, Union, Optional, NoReturn
+import logging
+
+
+logging.basicConfig(
+    level = logging.DEBUG,
+)
+LOG = logging.getLogger(Path(__file__).stem)
 
 
 @dataclass
@@ -37,10 +44,12 @@ def cap_playduration(
     jf_db_cur: sqlite3.Cursor,
     ) -> int:
     """
-    caps every playback entry to the runtime of the played item
+    caps every playback entry to the runtime of the played item,
+    but skips entries where PlaybackMethod is NULL
     
     note: this function does not automatically commits the updated playback durations
     """
+    global LOG
     
     item_runtime = jf_db_cur.execute("""
         SELECT PresentationUniqueKey AS ItemId, RunTimeTicks / 10000000 AS RunTimeSec
@@ -48,21 +57,26 @@ def cap_playduration(
         WHERE RunTimeTicks is NOT NULL
     """).fetchall()
     item_runtime_map = { i["ItemId"]: i["RunTimeSec"] for i in item_runtime }
+    LOG.info("found %s base items with set runtime", len(item_runtime))
     
     capplaydur_items: list[tuple] = []
     for entry in pb_db_cur.execute("""
-        SELECT ItemId, PlayDuration
+        SELECT ItemId, PlayDuration, PlaybackMethod
         FROM PlaybackActivity
+        WHERE PlaybackMethod is not NULL
     """).fetchall():
         if (runtime_sec := item_runtime_map.get(entry["ItemId"])) != None:
             if entry["PlayDuration"] > runtime_sec:
                 capplaydur_items.append((runtime_sec, entry["ItemId"]))
+                LOG.debug("capped playback duration of %ss of item id %s to runtime %ss", entry["PlayDuration"], entry["ItemId"], runtime_sec)
     
     updated_rowcount = pb_db_cur.executemany("""
         UPDATE PlaybackActivity
         SET PlayDuration = ?
         WHERE ItemId = ?
+        AND PlaybackMethod is not NULL
     """, capplaydur_items).rowcount
+    LOG.info("updated runtime of %s/%s items", updated_rowcount, len(capplaydur_items))
     
     return updated_rowcount
 
@@ -76,6 +90,7 @@ def compress_activity(
     
     note: this function does not automatically commits the deleted and inserted playback activities
     """
+    global LOG
 
     start_datestr = "2026-03-01"
     end_datestr = "2026-04-01"
@@ -96,7 +111,7 @@ def compress_activity(
     
     inserted_activity_rowcount = pb_db_cur.executemany(f"""
         INSERT INTO PlaybackActivity
-        VALUES (date('{start_datestr}'), :UserId, :ItemId, :ItemType, :ItemName, '', '', '', :TotalPlayDuration)
+        VALUES (date('{start_datestr}'), :UserId, :ItemId, :ItemType, :ItemName, NULL, NULL, NULL, :TotalPlayDuration)
     """, compressed_activity).rowcount
     
     return compressed_activity
@@ -106,8 +121,7 @@ def main() -> NoReturn:
     global CONFIG
 
     # load playback activity and jellyfin databases and create cursors
-    #  Use DictLike instead of Tuple for returns: sqlite3.Row
-    #  https://docs.python.org/3/library/sqlite3.html#how-to-create-and-use-row-factories
+    # use sqlite3.Row instead of tuples for returns: https://docs.python.org/3/library/sqlite3.html#how-to-create-and-use-row-factories
     pb_db_conn = sqlite3.connect(CONFIG.playback_activity_db)
     pb_db_conn.row_factory = sqlite3.Row
     pb_db_cur = pb_db_conn.cursor()
